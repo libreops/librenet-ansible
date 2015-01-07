02528754d6ac68a46e7f7ee9ec708b55728355bf
Achilleas Pipinellis <axilleas@axilleas.me>
2015-01-07 11:49:41 +0200

    Refine backup script.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
-
 set -e
 
-_date=$(date +%F-%s)
+DATE=$(date +%F-%s)
+DEST="/var/backup"
+LOGFILE="${DEST}/backup.log"
+TARFILE="${DEST}/diaspora-${DATE}.tar.gz"
+SQLFILE="${DEST}/diaspora-${DATE}.sql"
+UPLOADSDIR="/var/www/diaspora/public/uploads"
 
-mysqldump -uroot diaspora_production > /var/backup/diaspora-$_date.sql
+# Make sure backup dir exits
+[ ! -d "$DEST/latest" ] && mkdir -p "$DEST/latest"
 
-tar zcf /var/backup/diaspora-$_date.tar.gz \
-        /var/www/diaspora/public/uploads \
-        /var/backup/diaspora-$_date.sql
+mysqldump -uroot diaspora_production > $SQLFILE
 
-if [[ -f /var/backup/diaspora-$_date.tar.gz ]]; then
-  rm /var/backup/diaspora-$_date.sql
-fi
+[ -d "$UPLOADSDIR" ] && \
+tar zcf $TARFILE $UPLOADSDIR $SQLFILE || \
+tar zcf $TARFILE $SQLFILE
+
+echo -e "$(du -sh $TARFILE)\t$(md5sum $TARFILE | awk '{print $1}')" >> $LOGFILE
+
+[ -f $TARFILE ] && rm $SQLFILE
+
+rm -rf ${DEST}/latest/*tar.gz
+
+cp $TARFILE ${DEST}/latest/
