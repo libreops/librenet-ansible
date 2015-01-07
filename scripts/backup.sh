8e29e1bf599c9bbe104f6b71bb37a6a27f036f57
Achilleas Pipinellis <axilleas@axilleas.me>
2015-01-07 12:26:22 +0200

    Add comments to backup script.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -11,16 +11,24 @@ UPLOADSDIR="/var/www/diaspora/public/uploads"
 # Make sure backup dir exits
 [ ! -d "$DEST/latest" ] && mkdir -p "$DEST/latest"
 
+# Dump Diaspora's mysql database.
+# You must have /root/.my.cnf properly configured.
 mysqldump -uroot diaspora_production > $SQLFILE
 
+# Tar everything up and then gunzip it. If there is no uploads folder, just
+# include the database dump.
 [ -d "$UPLOADSDIR" ] && \
 tar zcf $TARFILE $UPLOADSDIR $SQLFILE || \
 tar zcf $TARFILE $SQLFILE
 
+# If backup completed successfully, remove the mysql dump.
+[ $? -eq 0 ] && rm $SQLFILE
+
+# Keep a simple backup log with size name and md5sum.
 echo -e "$(du -sh $TARFILE)\t$(md5sum $TARFILE | awk '{print $1}')" >> $LOGFILE
 
-[ -f $TARFILE ] && rm $SQLFILE
-
+# Remove everything in `latest/` dir.
 rm -rf ${DEST}/latest/*tar.gz
 
+# Copy tar backup to `latest/` dir. Hack to download latest backup via ansible.
 cp $TARFILE ${DEST}/latest/
