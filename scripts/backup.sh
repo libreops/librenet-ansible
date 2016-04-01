e87845eef6ec5cd37542f230e68d5b55dae2049c
Achilleas Pipinellis <axilleas@axilleas.me>
2016-04-01 14:29:45 +0300

    Add prosody files to backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,6 +7,7 @@ LOGFILE="${DEST}/backup.log"
 TARFILE="${DEST}/diaspora-${DATE}.tar.gz"
 SQLFILE="${DEST}/diaspora-${DATE}.psql"
 UPLOADSDIR="/var/www/diaspora/public/uploads"
+PROSODYDIR="/var/lib/prosody"
 
 # Make sure backup dir exits
 [ ! -d "$DEST/latest" ] && mkdir -p "$DEST/latest"
@@ -15,19 +16,16 @@ UPLOADSDIR="/var/www/diaspora/public/uploads"
 # You must have /home/diaspora/.pgpass properly configured.
 sudo -u diaspora -H pg_dump diaspora_production -f $SQLFILE
 
-# Tar everything up and then gunzip it. If there is no uploads folder, just
-# include the database dump.
-[ -d "$UPLOADSDIR" ] && \
-tar zcf $TARFILE $UPLOADSDIR $SQLFILE || \
-tar zcf $TARFILE $SQLFILE
+# Tar everything up and then gunzip it.
+tar zcf $TARFILE $UPLOADSDIR $PROSODYDIR $SQLFILE
 
-# If backup completed successfully, remove the mysql dump.
+# If backup completed successfully, remove the SQL dump.
 [ $? -eq 0 ] && rm $SQLFILE
 
 # Keep a simple backup log with size name and md5sum.
 echo -e "$(du -sh $TARFILE)\t$(md5sum $TARFILE | awk '{print $1}')" >> $LOGFILE
 
-# Remove everything in `latest/` dir.
+# Remove tars in `latest/` dir.
 rm -rf ${DEST}/latest/*tar.gz
 
 # Copy tar backup to `latest/` dir. Hack to download latest backup via ansible.
